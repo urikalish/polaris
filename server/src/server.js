6a55a8ffaeb6efa78566fabcd0acb4109efab784
kalish <uri.kalish@gmail.com>
2024-05-16 15:57:48 +0300

    server

--- server/src/server.js
+++ server/src/server.js
@@ -33,29 +33,49 @@ async function refreshGitHubData() {
         Authorization: `token ${GITHUB_AUTH_TOKEN}`,
     };
 
-    for (let page = 1; page <= 5; page++) {
-        let prs;
-        const url = `${GITHUB_BASE_URL}/api/v3/repos/${GITHUB_ORG_NAME}/${GITHUB_REPO_NAME}/pulls?state=all&per_page=100&page=${page}`;
+    const apiUrlBase = `${GITHUB_BASE_URL}/api/v3/repos/${GITHUB_ORG_NAME}/${GITHUB_REPO_NAME}`;
+
+    for (let page = 1; page <= 1; page++) {
+        const url = `${apiUrlBase}/pulls?state=all&per_page=100&page=${page}`;
         try {
-            const res = await axios.get(url, { headers });
-            prs = await res.data;
-            prs.forEach((pr) => {
+            let res = await axios.get(url, { headers });
+            const prs = await res.data;
+            for (let pr of prs) {
                 const prRecord = {
                     number: pr.number,
-                    htmlUrl: pr['html_url'],
-                    state: pr['merged_at'] ? 'merged' : pr['closed_at'] ? 'closed' : 'open',
+                    htmlUrl: pr.html_url,
+                    state: pr.merged_at ? 'merged' : pr.closed_at ? 'closed' : 'open',
                     title: pr.title,
                     branch: pr.head.ref,
-                    creator: pr.user['login'],
-                    reviewers: pr['requested_reviewers'] ? pr['requested_reviewers'].map((rr) => rr['login']) : [],
-                    assignees: pr['assignees'] ? pr['assignees'].map((rr) => rr['login']) : [],
+                    creator: pr.user.login,
+                    assignees: pr.assignees ? pr.assignees.map((a) => a.login) : [],
+                    reviewers: pr.requested_reviewers ? pr.requested_reviewers.map((rr) => rr.login) : [],
+                    reviews: [],
                 };
+
+                const url = `${apiUrlBase}/pulls/${prRecord.number}/reviews`;
+                const res = await axios.get(url, { headers });
+                const reviews = await res.data;
+                reviews.forEach((review) => {
+                    const reviewerName = review.user.login;
+                    if (!prRecord.reviewers.includes(reviewerName)) {
+                        prRecord.reviewers.push(reviewerName);
+                    }
+                    const r = prRecord.reviews.find((r) => r.user === reviewerName);
+                    if (r) {
+                        r.state = review.state.toLowerCase();
+                    } else {
+                        prRecord.reviews.push({ user: reviewerName, state: review.state.toLowerCase() });
+                    }
+                });
+
                 allPRs.push(prRecord);
-            });
+            }
         } catch (error) {
             console.log(error);
         }
     }
+
     return allPrs;
 }
 
