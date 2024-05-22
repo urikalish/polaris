c166a1132ea4444d5f3f80331ba24498ccaa2c29
kalish <uri.kalish@gmail.com>
2024-05-22 19:23:13 +0300

    theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -28,7 +28,7 @@ function App() {
     }, []);
 
     return (
-        <div className="height--100 overflow--hidden">
+        <div className={`height--100 overflow--hidden theme--${config?.uiTheme || 'bokeh'}`}>
             <AppBackground uiTheme={config?.uiTheme} />
             <div className="main-container">
                 <AppMasthead />
