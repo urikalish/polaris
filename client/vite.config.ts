730dd90034c78c02b0c36db31ce791a6a9eaef5a
kalish <uri.kalish@gmail.com>
2024-05-27 11:34:18 +0300

    don't minify the output

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         outDir: 'chrome-ext',
-        minify: true,
+        minify: false,
         rollupOptions: {
             input: {
                 popup: 'index.html',
