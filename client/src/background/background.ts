2f1ee634c40eb1d43d510fde5ad327adccca7fef
kalish <uri.kalish@gmail.com>
2024-06-09 18:38:54 +0300

    awaited reviews

--- client/src/background/background.ts
+++ client/src/background/background.ts
@@ -1,4 +1,4 @@
-async function getPrsFromServer(request: any) {
+async function getDataFromServer(request: any) {
     try {
         const res = await fetch(`${request.serverUrl}/${request.type}${request.params ? `?${request.params}` : ''}`);
         const responseObj = await res.json();
@@ -17,10 +17,46 @@ async function getPrsFromServer(request: any) {
 }
 
 chrome.runtime.onMessage.addListener((request: any, _sender: any, sendResponse: any) => {
-    if (['pull-requests'].includes(request.type)) {
-        getPrsFromServer(request).then((response) => {
+    if (request.type === 'pull-requests') {
+        getDataFromServer(request).then((response) => {
             sendResponse(response);
         });
         return true;
     }
 });
+
+const SECONDS_BEFORE_FIRST_UPDATE_AWAITED_REVIEWS = 10;
+const MINUTES_BETWEEN_UPDATE_AWAITED_REVIEWS = 1;
+
+function updateAwaitedReviews() {
+    const STORAGE_MAIN_KEY = 'polaris';
+    chrome.storage.local.get(STORAGE_MAIN_KEY, (data: any) => {
+        const { serverUrl, gitHubUserName } = data[STORAGE_MAIN_KEY];
+        if (!serverUrl || !gitHubUserName) {
+            return;
+        }
+        const request = {
+            type: 'awaited-reviews',
+            serverUrl,
+            params: `username=${gitHubUserName}`,
+        };
+        getDataFromServer(request).then((response) => {
+            if (response.error) {
+                console.log(response.error);
+                chrome.browserAction.setBadgeText({ text: '' });
+                return;
+            }
+            chrome.action.setBadgeBackgroundColor({ color: '#444' });
+            chrome.action.setBadgeText({ text: response.data.numberOfAwaitedReviews.toString() });
+            setTimeout(updateAwaitedReviews, MINUTES_BETWEEN_UPDATE_AWAITED_REVIEWS * 60 * 1000);
+        });
+    });
+}
+
+function init() {
+    setTimeout(() => {
+        updateAwaitedReviews();
+    }, SECONDS_BEFORE_FIRST_UPDATE_AWAITED_REVIEWS * 1000);
+}
+
+init();
